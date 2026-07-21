1e51c10498b4ad1a96a1acd95c59b57f777da25a
TaeHyun <thdh0215@gmail.com>
2026-07-21 15:50:21 +0900

    Add Oxford 5000 mobile PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oxford-5000-pwa-v1";
+const CACHE_NAME = "oxford-5000-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,6 +6,8 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./icons/icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
   "./data/words.json",
 ];
 
